e88b957776b43851f39b175022466de9465dc6d5
Jana Vitvarova <jana.vitvarova@tul.cz>
2022-03-30 16:10:52 +0200

    settery a atributy

---
+++ src/school/Zavodnik.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package school;
+
+/**
+ *
+ * @author 
+ */
+public class Zavodnik {
+    private String name;
+    private String surName;
+    private String run;
+    private int birthYear;
+    private boolean gender;
+    private String club;
+    private int startNum;
+    private int wave;
+    private boolean paid;
+    private int startTime;
+    private int endTime;
+    private int time;
+    private int position;
+    
+}
